2da291b412ef31b893e42b055ff4780766accd00
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-12-28 10:20:00 -0600

    Update and rename Item.js to itemModel.js

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -48,3 +48,19 @@ export const createItem = async (item) => {
   );
   return result.rows[0];
 };
+import mongoose from 'mongoose';
+
+const itemSchema = mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  name: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, default: 0 }, // Price in Pi
+  countInStock: { type: Number, required: true, default: 0 },
+  rating: { type: Number, default: 0 },
+  numReviews: { type: Number, default: 0 },
+}, { timestamps: true });
+
+const Item = mongoose.model('Item', itemSchema);
+export default Item;
