16a0deaae80d09edf7147c83e7ac5789918d42d6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-12-28 10:14:12 -0600

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,15 @@
+import express from 'express';
+import authRoutes from './authRoutes.js';
+import userRoutes from './userRoutes.js';
+import itemRoutes from './itemRoutes.js';
+import purchaseRoutes from './purchaseRoutes.js';
 
+const router = express.Router();
+
+// Mounting the specific sub-routers
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/items', itemRoutes);
+router.use('/purchases', purchaseRoutes);
+
+export default router;
